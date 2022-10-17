60298d4df2f8103ad6d2cc76ee4fefea1964965f
4000m <16733682@qq.com>
2022-10-17 15:02:46 +0800

    完成phone页面,继续监控画面页面.

--- app/src/main/java/com/bluesky/autojiahua/ui/monitor/MonitorFragment.java
+++ app/src/main/java/com/bluesky/autojiahua/ui/monitor/MonitorFragment.java
@@ -5,14 +5,19 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.bluesky.autojiahua.R;
+import com.bluesky.autojiahua.databinding.FragmentMonitorBinding;
 
 public class MonitorFragment extends Fragment {
+
+    private FragmentMonitorBinding mBinding;
+    private MonitorViewModel mMonitorViewModel;
 
     public static MonitorFragment newInstance() {
         return new MonitorFragment();
@@ -21,8 +26,21 @@ public class MonitorFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_monitor, container, false);
+        //return inflater.inflate(R.layout.fragment_monitor, container, false);
+        mBinding = FragmentMonitorBinding.inflate(inflater, container, false);
+        mMonitorViewModel = new ViewModelProvider(this).get(MonitorViewModel.class);
+        return mBinding.getRoot();
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mBinding = null;
+    }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        
+    }
 }
